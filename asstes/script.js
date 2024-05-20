9f851b629d0e1cc4bcb57e1001035b539241115b
sreeja <sreejabhojan518@gmail.com>
2024-05-20 14:41:07 +0530

    added js functions

---
+++ asstes/script.js
@@ -0,0 +1,15 @@
+function browsefav() {
+    location.replace("http://127.0.0.1:5500/index.html")
+}
+
+function favhmmainpage(){
+    location.replace("http://127.0.0.1:5500/index.html")
+}
+
+function signinhomepage(){
+    location.replace("http://127.0.0.1:5500/pages/signin.html")
+}
+
+function favouritehomepage(){
+    location.replace("http://127.0.0.1:5500/pages/favouritepage.html")
+}
